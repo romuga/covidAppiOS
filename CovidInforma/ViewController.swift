aaadd4c3e83c731676ff6e82aa74ad71a174ad68
Rosario Munoz <rossmg1996@gmail.com>
2020-12-08 20:41:25 -0600

    Initial Commit

---
+++ CovidInforma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidInforma
+//
+//  Created by Ross on 08/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
