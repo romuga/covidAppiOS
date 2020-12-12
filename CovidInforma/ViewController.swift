97adcd4a8d7282a48168bc42a091a74bed3f2752
Rosario Munoz <rossmg1996@gmail.com>
2020-12-11 22:17:30 -0600

    add cltypinglabel

--- CovidInforma/ViewController.swift
+++ CovidInforma/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensajeCovidLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mensajeCovidLabel.text = "Los coronavirus son una familia de virus que causan enfermedades (desde el resfriado común hasta enfermedades respiratorias más graves) y circulan entre humanos y animales. En este caso, se trata del SARS-COV2. Apareció en China en diciembre pasado y provoca una enfermedad llamada COVID-19, que se extendió por el mundo y fue declarada pandemia global por la Organización Mundial de la Salud."
+        mensajeCovidLabel.charInterval = 1.0
     }
 
 
---
+++ Pods/Target Support Files/Pods-CovidInforma/Pods-CovidInforma-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CovidInforma : NSObject
+@end
+@implementation PodsDummy_Pods_CovidInforma
+@end
